685ef790266ce71a0ef4bfee149d7d5fbdf3b908
Nathan Daniel Peterson <natperson@gmail.com>
2018-03-02 12:14:28 -0800

    solve anagram problem

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,43 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+const lower = (str) => {
+    return str.toLowerCase()
+}
+
+const alphaNumeric = (str) => {
+    return str.replace(/[^\w]/g, '')
+}
+
+const mapper = (str) => {
+    let obj = {}
+    for(char of str) {
+        obj[char] ? obj[char] += 1 : obj[char] = 1
+    }
+    return obj
+}
+
+const compare = (objA, objB) => {
+    for(let el in objA){
+        if(objA[el] !==objB[el]) return false
+    }
+    return true
+}
+
+const _pipe = (curr, acc) => (...args) => {
+    return acc(curr(...args))
+}
+
+const pipe = (...fns) => {
+    return fns.reduce(_pipe)
+}
+
+function anagrams(stringA, stringB) {
+    let formatter = pipe(alphaNumeric, lower, mapper)
+    let mapA = formatter(stringA)
+    let mapB = formatter(stringB)
+    if(compare(mapA, mapB) && compare(mapB, mapA)) return true
+    return false
+}
 
 module.exports = anagrams;
